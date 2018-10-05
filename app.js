ba7074f5fd1e31727f977bcac1ef4fb7a2af3124
s777610 <s777610@gmail.com>
2018-10-05 02:29:20 -0700

    Expose DOMstrings to global app controller

--- app.js
+++ app.js
@@ -8,11 +8,29 @@ var budgetController = (function () {
 
 
 
-
+// private
 var UIController = (function() {
-    // some code
 
-
+    var DOMstrings = {
+      inputType: '.add__type',
+      inputDescription: '.add__description',
+      inputValue: '.add__value',
+      inputBtn: '.add__btn'
+    };
+    
+    // expose it, public
+    return {
+      getInput: function() {
+        return {
+          type: document.querySelector(DOMstrings.inputType).value, // income or expenses
+          description: document.querySelector(DOMstrings.inputDescription).value,
+          value: document.querySelector(DOMstrings.inputValue).value
+        };
+      },
+      getDOMstrings: function() {
+        return DOMstrings;
+      }
+    };
 })();
 
 
@@ -21,22 +39,23 @@ var UIController = (function() {
 // global app controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
     var ctrlAddItem = function() {
       // 1. get input data
+      var input = UICtrl.getInput();
+      console.log(input);
       // 2. add the item to budget controller
       // 3. add the item to UI
       // 4. calculate the budget
       // 5. display the budget on UI
-      console.log('it works')
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
       }
     });
-
 })(budgetController, UIController);
 
 
