9acb11012908a649c2f0ca1ac02f6005c717f3ed
s777610 <s777610@gmail.com>
2018-10-06 12:59:54 -0700

    calculate the budget

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ var budgetController = (function () {
       this.value = value;
     };
 
+    var calculateTotal = function(type) {
+      var sum = 0;
+      data.allItems[type].forEach(function(cur) {
+        sum += cur.value;
+      });
+      data.totals[type] = sum;
+
+    };
+
     var data = {
       allItems: {
         exp: [],
@@ -20,7 +29,9 @@ var budgetController = (function () {
       totals: {
         exp: 0,
         inc: 0
-      }
+      },
+      budget: 0,
+      percentage: -1 // -1 is something is non-existent
     };
 
 
@@ -43,6 +54,32 @@ var budgetController = (function () {
         // push it into our data structure
         data.allItems[type].push(newItem);
         return newItem;
+      },
+
+      calculateBudget: function() {
+        // calculate total incom and expenses
+        calculateTotal('exp');
+        calculateTotal('inc');
+
+        // calculate the budget: income - expenses
+        data.budget = data.totals.inc - data.totals.exp;
+
+        // calculate the percentage of income that we spent
+        if (data.totals.inc > 0) {
+          data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+        } else {
+          data.percentage = -1;
+        }
+
+      },
+
+      getBudget: function() {
+        return {
+          budget: data.budget,
+          totalInc: data.totals.inc,
+          totalExp: data.totals.exp,
+          percentage: data.percentage
+        }
       },
 
       testing: function() {
@@ -139,10 +176,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
     var updateBudget = function() {
       // 1. calculate the budget
+      budgetCtrl.calculateBudget();
 
       // 2. return the budget
+      var budget = budgetCtrl.getBudget();
 
       // 3. display the budget on UI
+      console.log(budget);
     };
 
     var ctrlAddItem = function() {
@@ -152,7 +192,6 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       // if value > 0 and is number
       if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 2. add the item to budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. add the item to UI
